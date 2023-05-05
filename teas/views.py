e021107c65aecf5a159c0e803cb6b5e1aaced898
Kerynn Davis <kerynn.davis@gmail.com>
2023-05-05 09:18:21 -0700

    create post method for customer

--- teas/views.py
+++ teas/views.py
@@ -3,13 +3,26 @@ from .models import Tea
 from .models import Customer
 from .serializers import TeaSerializer
 from .serializers import CustomerSerializer
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
 
+@api_view(['GET'])
 def tea_list(request):
     teas = Tea.objects.all()
     serializer = TeaSerializer(teas, many=True)
     return JsonResponse({'teas': serializer.data})
 
+@api_view(['GET', 'POST'])
 def customer_list(request):
-    customers = Customer.objects.all()
-    serializer = CustomerSerializer(customers, many=True)
-    return JsonResponse({'customers': serializer.data})
+
+    if request.method == 'GET':
+        customers = Customer.objects.all()
+        serializer = CustomerSerializer(customers, many=True)
+        return JsonResponse({'customers': serializer.data})
+
+    if request.method == 'POST':
+        serializer = CustomerSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
