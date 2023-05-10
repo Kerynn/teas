873d2471ef70e584bc175e1649fa566c03bda59f
Kerynn Davis <kerynn.davis@gmail.com>
2023-05-10 13:38:27 -0700

    add subscription to admin site

--- teas/admin.py
+++ teas/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from .models import Tea
 from .models import Customer
+from .models import Subscription
 
 admin.site.register(Tea)
 admin.site.register(Customer)
+admin.site.register(Subscription)
