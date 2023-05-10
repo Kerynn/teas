5739bb7ec2496a28356754ce5afbd8e4ea1bf885
Kerynn Davis <kerynn.davis@gmail.com>
2023-05-10 13:18:05 -0700

    add subscription model and fields

--- teas/models.py
+++ teas/models.py
@@ -17,3 +17,11 @@ class Customer(models.Model):
 
     def __str__(self):
       return self.first_name + " " + self.last_name
+
+class Subscription(models.Model):
+    tea = models.ForeignKey(Tea, on_delete=models.CASCADE)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    status = models.CharField(max_length=200)
+    frequency = models.CharField(max_length=200)
